7112360eb318456a6874b2f8aecee9b63491a416
Guilherme Rios <gasrios@gmail.com>
2017-12-31 23:04:58 -0200

    Refactor to make it easier to extend/implement image editors

---
+++ src/main/java/com/github/gasrios/raw/Editor.java
@@ -0,0 +1,9 @@
+package com.github.gasrios.raw;
+
+import com.github.gasrios.raw.formats.ImageLSH;
+
+public interface Editor {
+
+	void edit(ImageLSH image);
+
+}
---
+++ src/main/java/com/github/gasrios/raw/ImageEditor.java
@@ -0,0 +1,47 @@
+/*
+ * © 2016 Guilherme Rios All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.github.gasrios.raw;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.github.gasrios.raw.formats.ImageLSH;
+import com.github.gasrios.raw.lang.TiffProcessorException;
+import com.github.gasrios.raw.processor.DngProcessor;
+import com.github.gasrios.raw.processor.TiffProcessorEngine;
+import com.github.gasrios.xmp.XMPException;
+
+public class ImageEditor extends DngProcessor<ImageLSH> {
+
+	private Editor editor;
+
+	public ImageEditor(ImageLSH image, Editor editor) {
+		super(image);
+		this.editor = editor;
+	}
+
+	@Override public void end() throws TiffProcessorException {
+		editor.edit(image);
+	}
+
+	public static void main(String[] args) throws IOException, ReflectiveOperationException, TiffProcessorException, XMPException {
+		new TiffProcessorEngine(
+			new FileInputStream(args[0]),
+			new ImageEditor(
+				new ImageLSH(),
+				(Editor) Class.forName(args[1]).newInstance()
+			)
+		).run();
+	}
+
+}
