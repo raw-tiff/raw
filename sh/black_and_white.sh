3297591fddee1de4558a6c13cfac4e5ed19036fa
Guilherme Rios <gasrios@gmail.com>
2018-07-08 18:05:32 -0300

    Moved examples to a separate project.

--- sh/black_and_white.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp ../bin:../lib/xmpcore-5.1.3.jar com.github.gasrios.raw.examples.BlackAndWhite $1
--- sh/read_metadata.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for file do java -cp ../bin:../lib/xmpcore-5.1.3.jar com.github.gasrios.raw.ReadMetadata $file; echo "--"; done
